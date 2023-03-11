ee5f2af9b77204a7fe1b9814a2206b98bfd8b898
AAWA-byt <mail@aaronwagner.earth>
2023-03-11 23:54:26 +0100

    client: :sparkles: chatbar with informations

---
+++ client/src/components/ChatBar.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+
+const ChatBar = ({ socket }) => {
+  // Define state to hold the list of active users
+  const [users, setUsers] = useState([]);
+
+  // Listen for 'newUserResponse' event from server and update state accordingly
+  useEffect(() => {
+    socket.on('newUserResponse', (data) => setUsers(data));
+  }, [socket, users]);
+
+  return (
+    <div className="chat__sidebar">
+      <h2>CHAT</h2>
+      <div>
+        <h4 className="chat__header">ACTIVE USERS</h4>
+        <div className="chat__users">
+          {/* Map through the 'users' state and render the list of active users */}
+          {users.map((user) => (
+            <p key={user.socketID}>{user.userName}</p>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBar;
+
+
