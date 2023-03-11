17659ae65a08f76500cd1a70a90bdb57aab04c8f
AAWA-byt <mail@aaronwagner.earth>
2023-03-11 23:53:48 +0100

    client: :sparkles: added route system

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// Import the required modules from the react-router-dom library
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Import the "Home" and "ChatPage" components from their respective files
+import Home from './components/Home';
+import ChatPage from './components/ChatPage';
+// Import the "socket.io-client" module to establish a connection to the server
+import socketIO from 'socket.io-client';
 
+// Use the "socket.io-client" module to create a socket object that connects to the server at "http://localhost:4000"
+const socket = socketIO.connect('http://localhost:4000');
+
+// Define a functional component called "App"
 function App() {
+  // Render the component hierarchy using the BrowserRouter, Routes, and Route components from react-router-dom
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          {/* Render the Home component when the path is "/" and pass the socket object as a prop */} 
+          <Route path="/" element={<Home socket={socket} />}></Route>
+          {/* Render the ChatPage component when the path is "/chat" and pass the socket object as a prop */}
+          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
+// Export the "App" component as the default export of this module
 export default App;
