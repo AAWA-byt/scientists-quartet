72f9abd24251cc969f63d591604bc28918bef7bd
AAWA-byt <mail@aaronwagner.earth>
2023-03-12 22:38:58 +0100

    client: :memo: changed title of page

--- client/src/components/ChatBar.js
+++ client/src/components/ChatBar.js
@@ -11,7 +11,7 @@ const ChatBar = ({ socket }) => {
 
   return (
     <div className="chat__sidebar">
-      <h2>Chat</h2>
+      <h2>Physiker Quartett</h2>
       <div>
         <h4 className="chat__header">ACTIVE USERS</h4>
         <div className="chat__users">
