574a12b00223bb6584654883989b6ac079273de9
AAWA-byt <mail@aaronwagner.earth>
2023-03-13 22:49:29 +0100

    client: :zap: added main header and footer for equal design

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -12,17 +12,38 @@ const Cards = ({ socket }) => {
 
     // render a div containing a header, user count and game status
     return (
-        <div className="chat__sidebar">
+        <><div className="chat__sidebar">
             <h2>Physiker Quartett</h2>
             <div>
                 <h4 className="chat__header">INFORMATIONS</h4>
                 <div className="chat__users">
                     <p id='playercount'><b>Players:</b> {users.length}/2</p>
                     <p id='gamestatus'><b>Gamestatus:</b> waiting</p>
-
                 </div>
             </div>
         </div>
+            <header className='chat__mainHeader'>
+                <p>Made with socket.io</p>
+                <button className='leaveChat__btn' >LEAVE GAME</button>
+            </header>
+
+            <div className='message__container'>
+
+            </div>
+
+            <div className="chat__footer">
+                <form className="form">
+                    {/* Input for message */}
+                    <input
+                        type="textarea"
+                        placeholder="Write message"
+                        className="message"
+                    // Event listener for the onKeyDown event to emit typing status
+                    />
+
+                </form>
+            </div>
+        </>
     );
 };
 
