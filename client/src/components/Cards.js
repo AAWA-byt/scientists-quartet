d41fa2d5ceca3b7b20903ce3b73a6e687f5536f8
AAWA-byt <mail@aaronwagner.earth>
2023-05-01 13:20:12 +0200

    client: :boom: Map distribution, stats display, button emojis

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,27 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from "react-router-dom"
-import albert_einstein from "../assets/albert_einstein.jpg"
+import { useNavigate } from 'react-router-dom';
+import albert_einstein from '../assets/albert_einstein.jpg';
 
-
-// define a functional component Cards that takes props
 const Cards = ({ socket }) => {
-    // use state hook to initialize users state to an empty array
     const [users, setUsers] = useState([]);
+    const [gotCards, setGotCards] = useState([]);
+    const [myCards, setMyCards] = useState([]);
+    const [cards1, setCards1] = useState([]);
+    const [cards2, setCards2] = useState([]);
+    const navigate = useNavigate();
 
-    const navigate = useNavigate()
+    const handleLeaveChat = () => {
+        localStorage.removeItem('userName');
+        localStorage.removeItem('userID');
+        navigate('/');
+        window.location.reload();
+    };
+
+    useEffect(() => {
+        socket.emit('first-user');
+
+    })
 
 
-    // useEffect hook to listen for 'newUserResponse' event from socket and update users state accordingly
     useEffect(() => {
         socket.on('newUserResponse', (data) => setUsers(data));
+        if (users.length === 1 && !localStorage.getItem('userID')) {
+            localStorage.setItem('userID', '1');
+            socket.emit('first-user');
+
+        } else if (users.length === 2) {
+            if (!localStorage.getItem('userID')) {
+                localStorage.setItem('userID', '2');
+                socket.emit('second-user');
+            }
+        }
     }, [socket, users]);
 
-    // This function is called when the "LEAVE CHAT" button is clicked
-    const handleLeaveChat = () => {
-        localStorage.removeItem("userName") // Remove the "userName" item from localStorage
-        navigate("/") // Navigate to the home page
-        window.location.reload() // Reload the page to reset the chat
-    }
+    useEffect(() => {
+        socket.on('send_first-user', (data) => {
+            setCards1(data);
+            if (localStorage.getItem('userID') === '1') {
+                setMyCards(data);
+            }
+        });
+    }, [socket]);
+
+    useEffect(() => {
+        socket.on('send_second-user', (data) => {
+            setCards2(data);
+            if (localStorage.getItem('userID') === '2') {
+                setMyCards(data);
+            }
+        });
+    }, [socket])
+
+
 
     // render a div containing a header, user count and game status
     return (
@@ -44,29 +78,30 @@ const Cards = ({ socket }) => {
                 <h1>Aktuelle Karte</h1>
                 <div className='cards__wrapper'>
                     <div className='image_container'>
-                        <img src={albert_einstein} alt="img" />
+                        <img src={myCards.length > 0 && myCards[0].photo} alt="img" />
                     </div>
                     <div className='stats_container'>
-                        <p id='stats_birth'><b>Geburtsdatum:</b> xxx</p>
-                        <p id='stats_iq'><b>IQ:</b> xxx</p>
-                        <p id='stats_awards'><b>Auszeichnungen:</b> xxx</p>
-                        <p id='stats_influence'><b>Einfluss:</b> xxx</p>
-                        <p id='stats_assets'><b>Vermögen:</b> xxx</p>
-                        <p id='stats_wiki'><b>Wikifaktor:</b> xxx</p>
+                        <p id='stats_name'><b>👤 Name:</b> {myCards.length > 0 && myCards[0].name}</p>
+                        <p id='stats_birth'><b>👶 Geburtsdatum:</b> {myCards.length > 0 && myCards[0].birth}</p>
+                        <p id='stats_iq'><b>🧠 IQ:</b> {myCards.length > 0 && myCards[0].iq}</p>
+                        <p id='stats_awards'><b>🏆 Auszeichnungen:</b> {myCards.length > 0 && myCards[0].awards}</p>
+                        <p id='stats_influence'><b>👑 Einfluss:</b> {myCards.length > 0 && myCards[0].influence}</p>
+                        <p id='stats_assets'><b>💵 Vermögen:</b> {myCards.length > 0 && myCards[0].assets}</p>
+                        <p id='stats_wiki'><b>📚 Wikifaktor:</b> {myCards.length > 0 && myCards[0].wiki}</p>
                     </div>
                 </div>
                 <div className='stats_buttons'>
-                    <button id='btn_birth'>test</button>
-                    <button id='btn_iq'>test</button>
-                    <button id='btn_awards'>test</button>
-                    <button id='btn_influence'>test</button>
-                    <button id='btn_assets'>test</button>
-                    <button id='btn_wiki'>test</button>
+                    <button id='btn_birth'>👶</button>
+                    <button id='btn_iq'>🧠</button>
+                    <button id='btn_awards'>🏆</button>
+                    <button id='btn_influence'>👑</button>
+                    <button id='btn_assets'>💵</button>
+                    <button id='btn_wiki'>📚</button>
                 </div>
             </div>
 
             <div className="cards__footer">
-                <p><b>Your Cards:</b> xx, <b>Turn:</b> xxxxx</p>
+                <p><b>Your Cards:</b> {myCards.length}, <b>Turn:</b> xxxxx</p>
             </div>
         </>
     );
