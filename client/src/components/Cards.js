0453825f929148ca997d5545af3f2d7f7ab67d7e
AAWA-byt <mail@aaronwagner.earth>
2023-05-01 13:43:46 +0200

    server: :memo: added comments

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,8 +1,10 @@
+//imports
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import albert_einstein from '../assets/albert_einstein.jpg';
 
 const Cards = ({ socket }) => {
+
+    // Define state variables for users, received cards, current user's cards, and cards for each player, and import useNavigate from react-router-dom.
     const [users, setUsers] = useState([]);
     const [gotCards, setGotCards] = useState([]);
     const [myCards, setMyCards] = useState([]);
@@ -10,6 +12,7 @@ const Cards = ({ socket }) => {
     const [cards2, setCards2] = useState([]);
     const navigate = useNavigate();
 
+    // Handle leaving the chat room
     const handleLeaveChat = () => {
         localStorage.removeItem('userName');
         localStorage.removeItem('userID');
@@ -17,18 +20,17 @@ const Cards = ({ socket }) => {
         window.location.reload();
     };
 
+    // Request cards from the server when the component mounts
     useEffect(() => {
         socket.emit('first-user');
-
     })
 
-
+    // Update the list of connected users when a new user joins or leaves the chat room
     useEffect(() => {
         socket.on('newUserResponse', (data) => setUsers(data));
         if (users.length === 1 && !localStorage.getItem('userID')) {
             localStorage.setItem('userID', '1');
             socket.emit('first-user');
-
         } else if (users.length === 2) {
             if (!localStorage.getItem('userID')) {
                 localStorage.setItem('userID', '2');
@@ -37,6 +39,7 @@ const Cards = ({ socket }) => {
         }
     }, [socket, users]);
 
+    // Update the cards for the first user when they receive them from the server
     useEffect(() => {
         socket.on('send_first-user', (data) => {
             setCards1(data);
@@ -46,6 +49,7 @@ const Cards = ({ socket }) => {
         });
     }, [socket]);
 
+    // Update the cards for the second user when they receive them from the server
     useEffect(() => {
         socket.on('send_second-user', (data) => {
             setCards2(data);
@@ -56,30 +60,35 @@ const Cards = ({ socket }) => {
     }, [socket])
 
 
-
-    // render a div containing a header, user count and game status
+    // Render a div containing a header, user count and game status
     return (
-        <><div className="cards__sidebar">
-            <h2>Physiker Quartett</h2>
-            <div>
-                <h4 className="cards__header">INFORMATIONS</h4>
-                <div className="cards__users">
-                    <p id='playercount'><b>Players:</b> {users.length}/2</p>
-                    <p id='gamestatus'><b>Gamestatus:</b> waiting</p>
+        <>
+            <div className="cards__sidebar">
+                <h2>Physiker Quartett</h2>
+                <div>
+                    <h4 className="cards__header">INFORMATIONS</h4>
+                    <div className="cards__users">
+                        <p id='playercount'><b>Players:</b> {users.length}/2</p>
+                        <p id='gamestatus'><b>Gamestatus:</b> waiting</p>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            {/* Header containing game title, made with statement and leave game button */}
             <header className='cards__mainHeader'>
                 <p>Made with socket.io</p>
                 <button className='leaveChat__btn' onClick={handleLeaveChat}>LEAVE GAME</button>
             </header>
 
+            {/* Container for the current card */}
             <div className='cards__container'>
                 <h1>Aktuelle Karte</h1>
                 <div className='cards__wrapper'>
+                    {/* Container for the card image */}
                     <div className='image_container'>
                         <img src={myCards.length > 0 && myCards[0].photo} alt="img" />
                     </div>
+                    {/* Container for the card stats */}
                     <div className='stats_container'>
                         <p id='stats_name'><b>👤 Name:</b> {myCards.length > 0 && myCards[0].name}</p>
                         <p id='stats_birth'><b>👶 Geburtsdatum:</b> {myCards.length > 0 && myCards[0].birth}</p>
@@ -90,6 +99,7 @@ const Cards = ({ socket }) => {
                         <p id='stats_wiki'><b>📚 Wikifaktor:</b> {myCards.length > 0 && myCards[0].wiki}</p>
                     </div>
                 </div>
+                {/* Container for the card stat buttons */}
                 <div className='stats_buttons'>
                     <button id='btn_birth'>👶</button>
                     <button id='btn_iq'>🧠</button>
@@ -100,12 +110,12 @@ const Cards = ({ socket }) => {
                 </div>
             </div>
 
+            {/* Footer containing player's card count and turn */}
             <div className="cards__footer">
                 <p><b>Your Cards:</b> {myCards.length}, <b>Turn:</b> xxxxx</p>
             </div>
         </>
     );
-};
-
-// export the Cards component as default
+}
+// Export the Cards component as default
 export default Cards;
